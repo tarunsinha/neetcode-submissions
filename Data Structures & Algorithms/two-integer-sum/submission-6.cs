e94b31723794457a694429cc9829f0467c1d3389
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-06-09 20:39:11 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        var dic = new Dictionary<int, int>();
+
+        for(int i =0; i< nums.Length;i++){
+            int sum = target - nums[i];
+
+            if(dic.ContainsKey(sum))
+                return new int[]{dic[sum], i};
+            else 
+                dic[nums[i]] = i;
+        }
+
+        return null;
+
+    }
+}
