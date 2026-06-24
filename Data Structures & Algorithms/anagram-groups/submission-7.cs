30a49f6e3d7e8083219d9c30985e9f4cbd67f7b7
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-06-24 20:17:13 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var dic = new Dictionary<string, List<string>>();
+
+        foreach(var word in strs){
+            var chCount = new int[26];
+            foreach(var ch in word){
+                chCount[ch-'a']++;
+            }
+
+            var key = string.Join('|', chCount);
+
+            if(!dic.ContainsKey(key))
+                dic[key] = new List<string>();
+            dic[key].Add(word);
+        }
+
+        return dic.Values.ToList();
+    }
+}
