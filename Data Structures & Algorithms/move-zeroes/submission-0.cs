fabb69f522405921b31dac46432f4d52a2b8569a
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-06-25 21:41:21 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        var insPos = 0;
+        var n = nums.Length;
+        for(int i=0; i< n;i++){
+            if(nums[i] != 0 ){
+                (nums[insPos], nums[i]) = (nums[i], nums[insPos]);
+                insPos++;
+            }
+        }
+    }
+}
