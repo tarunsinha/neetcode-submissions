060a13b9c070431ab4a47454a679a29cc1109577
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-08 00:18:14 +0530

    Add: longest-consecutive-sequence - submission-15

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-15.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        // Brute Force solution
+        Array.Sort(nums);
+        int maxCount = 1;
+        if (nums.Length == 0)
+            return 0;
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            var diff = nums[i] - nums[i - 1];
+            if (diff == 1) {
+                count++;
+                maxCount = Math.Max(count, maxCount);
+            } else if (diff > 1)
+                count = 1;
+        }
+        return maxCount;
+    }
+}
