cbe68f0379bd777dca10e84c11291b68cd8aa3ba
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-08 22:16:48 +0530

    Add: longest-consecutive-sequence - submission-18

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-18.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        /* Brute Force
+        - Sort array
+        - if Array is empty return 0 (edge case)
+        - iterate and check if diff is equal to 1?
+        - take max count
+        - if diff > 1, reset count to 1
+        - return max count
+        */
+        if (nums.Length == 0)
+            return 0;
+        Array.Sort(nums);
+        int maxCount = 1;
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            var diff = nums[i] - nums[i - 1];
+            if (diff == 1) {
+                count++;
+                maxCount = Math.Max(count, maxCount);
+            } else if (diff > 1)
+                count = 1;
+        }
+        return maxCount;
+    }
+}
