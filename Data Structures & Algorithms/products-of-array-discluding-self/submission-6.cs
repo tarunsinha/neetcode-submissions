d05790f99bd483b35544c9f4824940aae6780937
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-10 23:29:46 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public string Encode(IList<string> strs) {
+        StringBuilder sb = new();
+        foreach (var s in strs) sb.Append(s.Length).Append('#').Append(s);
+
+        return sb.ToString();
+    }
+    // 5#hello5#World
+    public List<string> Decode(string s) {
+        /*
+        - start from 0 -> till you find the separator ('#')
+        - find the substring to get the length of the word in this case 5
+        - move i -> j+1 to point at 'h'
+        - move j to end -> j+length
+        - add into a list substring from i -> length of word
+        - move i -> j so that i now points at seconf 5
+        */
+        int i = 0;
+        var res = new List<string>();
+        while (s.Length > i) {
+            int j = i;
+            while (s[j] != '#') j++;
+
+            int length = int.Parse(s.Substring(i, j - i));  // j-i to control overflow
+
+            i = j + 1;
+            j = i + length;
+            res.Add(s.Substring(i, length));
+            i = j;
+        }
+        return res;
+    }
+}
