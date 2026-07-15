4f175969c580afe8714e993d681589ffb935a5b9
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-15 23:37:06 +0530

    Add: buy-and-sell-crypto - submission-19

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-19.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int l = 0, r = 1, maxPro = 0;
+
+        while (r < prices.Length) {
+
+            if (prices[l] < prices[r]) {
+
+                maxPro = Math.Max(maxPro, prices[r] - prices[l]);
+
+            } else
+
+                l = r;
+
+            r++;
+
+        }
+        
+        return maxPro;
+    }
+}
