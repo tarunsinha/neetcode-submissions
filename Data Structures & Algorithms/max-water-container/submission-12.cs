a5627cdd3b2788566555da4785922bdafe07ffb9
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-15 23:16:28 +0530

    Add: max-water-container - submission-12

---
+++ Data Structures & Algorithms/max-water-container/submission-12.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int l = 0, r = heights.Length - 1, maxArea = 0;
+        while (l < r) {
+            int height = Math.Min(heights[l], heights[r]);
+            int width = r - l;
+            int area = height * width;
+            maxArea = Math.Max(area, maxArea);
+
+            if (heights[l] < heights[r])
+                l++;
+            else
+                r--;
+        }
+        return maxArea;
+    }
+}
