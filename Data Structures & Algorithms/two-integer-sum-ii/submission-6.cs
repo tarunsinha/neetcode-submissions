9b6104a1c1fc79c0ae9ed0da62cfc43137829de7
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-07-16 22:41:07 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int l = 0, r = nums.Length - 1;
+
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum < target)
+                l++;
+            else if (sum > target)
+                r--;
+            else {
+                return new int[] { l + 1, r + 1 };
+            }
+        }
+
+        return null;
+    }
+}
