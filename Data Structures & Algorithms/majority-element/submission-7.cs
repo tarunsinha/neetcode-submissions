8dc31fb730d710955271408adad06c0c4c426b4d
Tarun Sinha <tarunsinha.m2009@gmail.com>
2026-09-01 15:26:25 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int count = 0, candidate = -1;
+
+        foreach (var n in nums) {
+            if (count == 0) {
+                candidate = n;
+                count = 1;
+            } else if (candidate == n)
+                count += 1;
+            else
+                count -= 1;
+        }
+
+        return candidate;
+    }
+}
